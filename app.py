0f900fa7fd57d87687a2fe4ae81ec9f8a63f80ae
Londunic <elondonoc@unal.edu.co>
2023-07-09 20:48:41 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/upload', methods=['POST'])
+def upload_csv():
+    # Implementation for uploading and processing CSV files
+    file = request.files['file']
+    # Process the CSV file data
+
+    return 'CSV file uploaded successfully'
+
+@app.route('/batch-insert', methods=['POST'])
+def batch_insert():
+    # Implementation for batch insertion of transactions
+    data = request.json
+    # Process the batch insertion data
+
+    return 'Batch insertion completed'
+
+if __name__ == '__main__':
+    app.run()
